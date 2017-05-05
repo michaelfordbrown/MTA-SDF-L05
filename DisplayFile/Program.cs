af0de6445629e8b57f051d10218633ebbe5d9e6e
Michael Brown <michaelfordbrown@hotmail.com>
2017-05-05 23:44:39 +0100

    Create A Console Application

---
+++ DisplayFile/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace DisplayFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 1)
+                return;
+
+            string[] lines = File.ReadAllLines(args[0]);
+
+            foreach (string item in lines)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("Press Any Key To Exit");
+            Console.ReadKey();
+        }
+    }
+}
